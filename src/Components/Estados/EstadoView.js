04a94eac808d6dcdb67564a75e044c4dca2767b4
Mateo <mvalenciaminota@gmail.com>
2022-06-12 16:00:30 -0500

    Se añade Estadoservice y se actualiza EstadoView

--- src/Components/Estados/EstadoView.js
+++ src/Components/Estados/EstadoView.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getEstados } from '../../Services/EstadosService'
 
 export const EstadoView = () => {
+
+
+
+  const [estados, setEstados] = useState([]);
+
+  const listarEstados = async () => {
+    try{
+      const {data} = await getEstados();
+      setEstados(data);
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    listarEstados();
+  },[]);
+
+  listarEstados();
+
   return (
-    <div>EstadoView</div>
+    <div className="container-fluid">
+    <div className="row row-cols-1 row-cols-md-2 g-4">
+        {
+           estados.map((estado) => {
+            return (
+              <div className="col" key={estado._id}>
+                <div className="card">
+                  <img src={estado.foto} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{estado.nombre}</h5>
+                    <p className="card-text">{estado.estado}</p>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+    </div>
+  </div>
   )
 }
