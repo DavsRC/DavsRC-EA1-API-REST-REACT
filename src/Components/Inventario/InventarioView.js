e383476f9b2268b1196921c6865c0b01e3cea2ae
Davinson Rincon <davinson.rincon@est.iudigital.edu.co>
2022-06-12 18:56:21 -0500

    Se agregan CSS

--- src/Components/Inventario/InventarioView.js
+++ src/Components/Inventario/InventarioView.js
@@ -23,8 +23,8 @@ export const InventarioView = () => {
   listarInventarios();
 
   return (
-    <div className="container-fluid">
-        <div className="row row-cols-1 row-cols-md-2 g-4">
+    <div className="container">
+        <div className="mt-2 mb-2 row row-cols-1 row-cols-md-4 g-4">
             {
                inventarios.map((inventario) => {
                 return <InventarioCard key={inventario._id} inventario={inventario}/>
