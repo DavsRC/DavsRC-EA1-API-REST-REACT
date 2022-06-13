91df85d066997b4040275346d56c8443e347e859
Mateo <mvalenciaminota@gmail.com>
2022-06-12 19:12:37 -0500

    Se añade boton para añdir inventario

--- src/Components/Inventario/InventarioView.js
+++ src/Components/Inventario/InventarioView.js
@@ -1,36 +1,37 @@
-import React, { useState, useEffect } from 'react'
-import { getInventarios } from '../../Services/InventarioService'
-import {InventarioCard} from './InventarioCard'
+import React, { useState, useEffect } from "react";
+import { getInventarios } from "../../Services/InventarioService";
+import { InventarioCard } from "./InventarioCard";
 
 export const InventarioView = () => {
-
   const [inventarios, setInventarios] = useState([]);
 
   const listarInventarios = async () => {
-    try{
-      const {data} = await getInventarios();
+    try {
+      const { data } = await getInventarios();
       setInventarios(data);
-    }
-    catch(error){
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     listarInventarios();
-  },[]);
+  }, []);
 
   listarInventarios();
 
   return (
     <div className="container">
-        <div className="mt-2 mb-2 row row-cols-1 row-cols-md-4 g-4">
-            {
-               inventarios.map((inventario) => {
-                return <InventarioCard key={inventario._id} inventario={inventario}/>
-              })
-            }
-        </div>
+      <div className="mt-2 mb-2 row row-cols-1 row-cols-md-4 g-4">
+        {inventarios.map((inventario) => {
+          return (
+            <InventarioCard key={inventario._id} inventario={inventario} />
+          );
+        })}
       </div>
-  )
-}
+      <button className="btn btn-primary fab">
+        <i className="fa-solid fa-plus"></i>
+      </button>
+    </div>
+  );
+};
