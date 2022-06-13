6174c9a9144bf04acce3db2722e58aa4513d1b79
Mateo <mvalenciaminota@gmail.com>
2022-06-12 20:06:48 -0500

    Header Modal

--- src/Components/Inventario/InventarioNew.js
+++ src/Components/Inventario/InventarioNew.js
@@ -1,3 +1,23 @@
 import React from "react";
 
-
+export const InventarioNew = ({ handleOpenModal }) => {
+  return (
+    <div className="sideBar">
+      <div className="container-fluiod">
+        <div className="row">
+          <div className="col">
+            <div className="sidebar-header">
+              <h3>Nuevo inventario</h3>
+              <i class="fa-solid fa-xmark" onClick={handleOpenModal}></i>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <hr />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/Components/Inventario/InventariosNew.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const InventariosNew = () => {
-  return (
-    <div className='sideBar'>
-      <h1>Nuevo inventario</h1>
-    </div>
-  )
-}
